48a69039e579d00193cea6017be2adeff5a26e99
a00 <a00@ruppindom>
2019-04-30 11:11:27 +0300

    elemets

--- Lecture 01/demo2/App.js
+++ Lecture 01/demo2/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Button, ThemeProvider } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 export default class App extends React.Component {
   constructor(props) {
@@ -21,17 +24,53 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <ThemeProvider>
         <View style={styles.container}>
-          <Text style={{ backgroundColor: 'red' }}>demo ruppin</Text> 
+          <View style={styles.container}>
+            <Text style={{ backgroundColor: 'red' }}>demo~{"\n"} ruppin</Text>
+          </View>
+          <View style={styles.container}>
+            <TextInput onChangeText={this.txtNameCahnged} ></TextInput>
+          </View>
+          <View style={styles.container}>
+            <Button title='hello btn' onPress={this.btnHello}></Button>
+          </View>
+          <View style={styles.container}>
+            <Button
+              icon={
+                <Icon
+                  name="arrow-right"
+                  size={15}
+                  color="white"
+                />
+              }
+              title="Button with icon component"
+              type='outline'
+            />
+            <Button
+              icon={
+                <Icon
+                  name="arrow-right"
+                  size={15}
+                  color="white"
+                />
+              }
+              title="Button with icon component"
+            />
+            <Button
+              icon={
+                <Icon
+                  name="arrow-right"
+                  size={15}
+                  color="white"
+                />
+              }
+              iconRight
+              title="Button with right icon"
+            />
+          </View>
         </View>
-        <View style={styles.container}>
-          <TextInput onChangeText={this.txtNameCahnged} ></TextInput> 
-        </View>
-        <View style={styles.container}>
-          <Button title='hello btn' onPress={this.btnHello}></Button>
-        </View>
-      </View>
+      </ThemeProvider>
     );
   }
 }
@@ -43,7 +82,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     fontSize: 30,
-    borderWidth:2,
-    borderColor:'black'
+    borderWidth: 2,
+    borderColor: 'black',
+    margin: 10,
+    padding: 5
   },
 });
